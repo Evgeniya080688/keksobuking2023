53fc2f9b7b5ff7f139997ecc94dc9bd35f1b8337
PaladinaEug <paladina.em1@kvt.su>
2023-02-08 13:42:33 +0300

    Инициализация проекта

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+const res =getRandomNum(5, 88);
+console.log(res);
