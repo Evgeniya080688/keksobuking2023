d93db463a6e0c3567985f4c2835aeee4505482d8
PaladinaEug <paladina.em1@kvt.su>
2023-02-08 16:15:19 +0300

    Добавлены функции для генерирования случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 function getRandomNum(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  if (min > max) {
+    const oldMin = min;
+    min = max;
+    max = oldMin;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const res =getRandomNum(5, 88);
+const res = getRandomNum(25, 5);
 console.log(res);
+
+function getCoordinates(min, max, signs) {
+
+  if (min > max) {
+    const oldMin = min;
+    min = max;
+    max = oldMin;
+  }
+
+  if (Math.random() < 0.5) {
+    return ((1-Math.random()) * (max - min) + min).toFixed(signs);
+  }
+
+  return  (Math.random() * (max - min) + min).toFixed(signs);
+
+}
+console.log(getCoordinates(1.53, 4.33, 2));
