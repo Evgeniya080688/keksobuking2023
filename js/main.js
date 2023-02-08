418aa131bc7f30cc78c18eaa36d065e1da044b95
PaladinaEug <paladina.em1@kvt.su>
2023-02-08 20:48:59 +0300

    Добавлены проверки для функций

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-function checkParam(param) {
-  if (param >=0 && typeof(param) === 'number' && param!== undefined) {
-    return true;
-  }
-  return false;
-}
-
 function getRandomNum(min, max) {
-  if (!checkParam(min) || !checkParam(max)) {
-    return new Error('Введенные параметры некорректны!');
-  } else {
+  try {
+    if (max === undefined) {
+      //если ввели только 1 параметр
+      max = min;
+      min = 0;
+    }
+    if (min === undefined) {
+      //если ввели только 1 параметр
+      throw new Error('Необходимо ввести хотя бы один параметр');
+    }
+    if (!Number(max) || !Number(max)) {
+      //если ввели не число
+      throw new Error('Введенные параметры должны быть числами');
+    }
+    if (max < 0 || min < 0) {
+      //если ввели отрицательное число
+      throw new Error('Числа диапазона должны быть положительными');
+    }
     min = Math.ceil(min);
     max = Math.floor(max);
 
@@ -18,26 +26,45 @@ function getRandomNum(min, max) {
       max = oldMin;
     }
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  } catch (err) {
+    console.error(err); // в консоль попадает сообщение об ошибке и стек ошибки
   }
 }
-const res = getRandomNum(-15);
+
+const res = getRandomNum(6);
 console.log(res);
 
 function getCoordinates(min, max, signs) {
-  if (!checkParam(min) || !checkParam(max) || !checkParam(signs)) {
-    return new Error('Введенные параметры некорректны!');
-  } else {
+  try {
     if (min > max) {
       const oldMin = min;
       min = max;
       max = oldMin;
     }
     signs = Math.round(signs);
-
+    if (min === undefined) {
+      throw new Error('Необходимо ввести хотя бы одно число');
+    }
+    if (max === undefined) {
+      max = min;
+      min = 0;
+      signs = 2;
+    }
+    if (min < 0 || max < 0 || signs < 0 ) {
+      throw new Error('Параметры должны быть положительными');
+    }
+    if (!Number(max) || !Number(max) || !Number(signs)) {
+      //если ввели не число
+      throw new Error('Введенные параметры должны быть числами');
+    }
     if (Math.random() < 0.5) {
       return ((1 - Math.random()) * (max - min) + min).toFixed(signs);
     }
     return (Math.random() * (max - min) + min).toFixed(signs);
+  } catch (err) {
+    console.error(err); // в консоль попадает сообщение об ошибке и стек ошибки
   }
 }
-console.log(getCoordinates(1.53,  1, 2));
+
+const res2 = getCoordinates(15);
+console.log(res2);
