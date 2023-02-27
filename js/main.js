654ba485c499d103c8b12ea674e50cc9e9745782
PaladinaEug <paladina.em1@kvt.su>
2023-02-27 14:20:42 +0300

    Функции изменены в соответствии с примером

--- js/main.js
+++ js/main.js
@@ -1,70 +1,16 @@
-function getRandomNum(min, max) {
-  try {
-    if (max === undefined) {
-      //если ввели только 1 параметр
-      max = min;
-      min = 0;
-    }
-    if (min === undefined) {
-      //если ввели только 1 параметр
-      throw new Error('Необходимо ввести хотя бы один параметр');
-    }
-    if (!Number(max) || !Number(max)) {
-      //если ввели не число
-      throw new Error('Введенные параметры должны быть числами');
-    }
-    if (max < 0 || min < 0) {
-      //если ввели отрицательное число
-      throw new Error('Числа диапазона должны быть положительными');
-    }
-    min = Math.ceil(min);
-    max = Math.floor(max);
+// Функция взята из интернета
+// Источник - https://up.htmlacademy.ru/profession/react-lite/2/lite-javascript/2/tasks/6
 
-    if (min > max) {
-      const oldMin = min;
-      min = max;
-      max = oldMin;
-    }
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } catch (err) {
-    console.error(err); // в консоль попадает сообщение об ошибке и стек ошибки
-  }
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-const res = getRandomNum(6);
-console.log(res);
-
-function getCoordinates(min, max, signs) {
-  try {
-    if (min > max) {
-      const oldMin = min;
-      min = max;
-      max = oldMin;
-    }
-    signs = Math.round(signs);
-    if (min === undefined) {
-      throw new Error('Необходимо ввести хотя бы одно число');
-    }
-    if (max === undefined) {
-      max = min;
-      min = 0;
-      signs = 2;
-    }
-    if (min < 0 || max < 0 || signs < 0 ) {
-      throw new Error('Параметры должны быть положительными');
-    }
-    if (!Number(max) || !Number(max) || !Number(signs)) {
-      //если ввели не число
-      throw new Error('Введенные параметры должны быть числами');
-    }
-    if (Math.random() < 0.5) {
-      return ((1 - Math.random()) * (max - min) + min).toFixed(signs);
-    }
-    return (Math.random() * (max - min) + min).toFixed(signs);
-  } catch (err) {
-    console.error(err); // в консоль попадает сообщение об ошибке и стек ошибки
-  }
+function getRandomPositiveFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
 }
-
-const res2 = getCoordinates(15);
-console.log(res2);
