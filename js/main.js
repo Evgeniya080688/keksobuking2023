f34dc272adaa0d60eff3031da6d35371618d8e13
PaladinaEug <paladina.em1@kvt.su>
2023-03-02 10:54:16 +0300

    Функции генерации случайных чисел заменены на самостоятельно разработанные

--- js/main.js
+++ js/main.js
@@ -1,24 +1,89 @@
 // Функция взята из интернета
 // Источник - https://up.htmlacademy.ru/profession/react-lite/2/lite-javascript/2/tasks/6
 
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+// const getRandomPositiveInteger = (a, b) => {
+//   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+//   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+//   const result = Math.random() * (upper - lower + 1) + lower;
+//   return Math.floor(result);
+// };
+//
+// const getRandomPositiveFloat = (a, b, digits = 1) => {
+//   const lower = Math.min(Math.abs(a), Math.abs(b));
+//   const upper = Math.max(Math.abs(a), Math.abs(b));
+//   const result = Math.random() * (upper - lower) + lower;
+//   return +result.toFixed(digits);
+// };
+
+const getRandomNum = (min, max) => {
+  try {
+    if (max === undefined) {
+      //если ввели только 1 параметр
+      max = min;
+      min = 0;
+    }
+    if (min === undefined) {
+      //если ввели только 1 параметр
+      throw new Error('Необходимо ввести хотя бы один параметр');
+    }
+    if (!Number(max) || !Number(max)) {
+      //если ввели не число
+      throw new Error('Введенные параметры должны быть числами');
+    }
+    if (max < 0 || min < 0) {
+      //если ввели отрицательное число
+      throw new Error('Числа диапазона должны быть положительными');
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    if (min > max) {
+      const oldMin = min;
+      min = max;
+      max = oldMin;
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } catch (err) {
+    console.error(err); // в консоль попадает сообщение об ошибке и стек ошибки
+  }
 };
 
-const getRandomPositiveFloat = (a, b, digits = 1) => {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
-  const result = Math.random() * (upper - lower) + lower;
-  return +result.toFixed(digits);
+const getCoordinates = (min, max, signs = 1) => {
+  try {
+    if (min > max) {
+      const oldMin = min;
+      min = max;
+      max = oldMin;
+    }
+    signs = Math.round(signs);
+    if (min === undefined) {
+      throw new Error('Необходимо ввести хотя бы одно число');
+    }
+    if (max === undefined) {
+      max = min;
+      min = 0;
+      signs = 2;
+    }
+    if (min < 0 || max < 0 || signs < 0 ) {
+      throw new Error('Параметры должны быть положительными');
+    }
+    if (!Number(max) || !Number(max) || !Number(signs)) {
+      //если ввели не число
+      throw new Error('Введенные параметры должны быть числами');
+    }
+    if (Math.random() < 0.5) {
+      return ((1 - Math.random()) * (max - min) + min).toFixed(signs);
+    }
+    return (Math.random() * (max - min) + min).toFixed(signs);
+  } catch (err) {
+    console.error(err); // в консоль попадает сообщение об ошибке и стек ошибки
+  }
 };
 
 const SIMILAR_ADVERT_COUNT = 10;
 
 const getRandomArrayElement = (elements) => {
-  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+  return elements[getRandomNum(0, elements.length - 1)];
 };
 
 const getPicture = (number) => {
@@ -79,8 +144,8 @@ const PHOTOS = [
 
 const getLocation = () => {
   return {
-    lat: getRandomPositiveFloat(35.65000, 35.70000),
-    lng: getRandomPositiveFloat(139.70000, 139.80000),
+    lat: getCoordinates(35.65000, 35.70000),
+    lng: getCoordinates(139.70000, 139.80000),
   };
 };
 
@@ -88,15 +153,15 @@ const createOffer = (location) => {
   return {
     title: getRandomArrayElement(TITLES),
     address: `{${location.lat}}, {${location.lng}}`,
-    price: getRandomPositiveInteger(100, 10000),
+    price: getRandomNum(100, 10000),
     type: getRandomArrayElement(TYPES),
-    rooms: getRandomPositiveInteger(0, 10),
-    guests: getRandomPositiveInteger(1,100),
+    rooms: getRandomNum(1, 10),
+    guests: getRandomNum(1,100),
     checkin: getRandomArrayElement(CHEKS),
     checkout: getRandomArrayElement(CHEKS),
-    features: FEATURES.slice(getRandomPositiveInteger(0, FEATURES.length - 1) , getRandomPositiveInteger(1, FEATURES.length - 1)),
+    features: FEATURES.slice(getRandomNum(0, FEATURES.length - 1) , getRandomNum(1, FEATURES.length - 1)),
     description: getRandomArrayElement(DESCRIPTIONS),
-    photos: PHOTOS.slice(0, getRandomPositiveInteger(1, FEATURES.length - 1)),
+    photos: PHOTOS.slice(0, getRandomNum(1, FEATURES.length - 1)),
   };
 };
 
