89da8516a38ba451bd7e7f6ab54591a4b35d5a36
PaladinaEug <paladina.em1@kvt.su>
2023-04-24 15:29:21 +0300

    Первые настройки для валидации формы

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { listAdvert } from './data.js';
 import { renderAdverts } from './cards.js';
-import { activateForm, desactivateForm } from './formact.js';
+import { activateForm, desactivateForm } from './form-use.js';
 
 const advertisements = listAdvert(10);
 console.log(advertisements);
